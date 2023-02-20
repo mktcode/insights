113d9394e21fc2f0326c99a59f1745e93a6e9403
mktcode <kontakt@markus-kottlaender.de>
2023-02-20 16:55:40 +0100

    count stars and forks and followers

--- packages/insights/src/index.ts
+++ packages/insights/src/index.ts
@@ -1,5 +1,73 @@
-export function calculateAuthorScore(author: any) {
+export type Author = {
+  login: string;
+  createdAt: string;
+  followers: {
+    totalCount: number;
+    nodes: {
+      repositories: {
+        nodes: {
+          stargazerCount: number;
+          forkCount: number;
+        }[]
+      }
+      followers: {
+        totalCount: number;
+      }
+    }[]
+  }
+  repositories: {
+    nodes: {
+      stargazerCount: number;
+      forkCount: number;
+    }[]
+  }
+  pullRequests: {
+    totalCount: number;
+    nodes: {
+      merged: boolean;
+      headRepository: {
+        stargazerCount: number;
+        forkCount: number;
+      } | null;
+    }[]
+  }
+}
+
+export function calculateAuthorScore(author: Author) {
+  const forkCount = author.repositories.nodes.reduce(
+    (acc, repo) => acc + repo.forkCount,
+    0
+  );
+  const followersForkCount = author.followers.nodes.reduce(
+    (acc, follower) => acc + follower.repositories.nodes.reduce(
+      (acc, repo) => acc + repo.forkCount,
+      0
+    ),
+    0
+  );
+
+  const stargazerCount = author.repositories.nodes.reduce(
+    (acc, repo) => acc + repo.stargazerCount,
+    0
+  );
+  const followersStargazerCount = author.followers.nodes.reduce(
+    (acc, follower) => acc + follower.repositories.nodes.reduce(
+      (acc, repo) => acc + repo.stargazerCount,
+      0
+    ),
+    0
+  );
+  
+  const followersFollowerCount = author.followers.nodes.reduce(
+    (acc, follower) => acc + follower.followers.totalCount,
+    0
+  );
+
   return {
-    authorScore: 1000
+    forkCount,
+    followersForkCount,
+    stargazerCount,
+    followersStargazerCount,
+    followersFollowerCount,
   };
 }
--- packages/insights/tests/index.test.ts
+++ packages/insights/tests/index.test.ts
@@ -4,9 +4,23 @@ import GITHUB_AUTHOR_MOCK from './mocks/github-author.json';
 
 describe('author module', () => {
   test('calculates score correctly', () => {
-    const expectedAuthorScore = 1000;
-    const { authorScore } = calculateAuthorScore(GITHUB_AUTHOR_MOCK.data.viewer);
+    const expectedForkCount = 753;
+    const expectedFollowersForkCount = 11368;
+    const expectedStargazerCount = 2389;
+    const expectedFollowersStargazerCount = 18243;
+    const expectedFollowersFollowerCount = 17949;
+    const {
+      forkCount,
+      followersForkCount,
+      stargazerCount,
+      followersStargazerCount,
+      followersFollowerCount,
+    } = calculateAuthorScore(GITHUB_AUTHOR_MOCK.data.viewer);
 
-    expect(authorScore).toBe(expectedAuthorScore);
+    expect(forkCount).toBe(expectedForkCount);
+    expect(followersForkCount).toBe(expectedFollowersForkCount);
+    expect(stargazerCount).toBe(expectedStargazerCount);
+    expect(followersStargazerCount).toBe(expectedFollowersStargazerCount);
+    expect(followersFollowerCount).toBe(expectedFollowersFollowerCount);
   });
 });
