0b67dc7e140dedf306b38957983349449587d23f
mktcode <kontakt@markus-kottlaender.de>
2023-02-20 18:29:25 +0100

    further project setup

--- app/client/.eslintrc.cjs
+++ app/client/.eslintrc.cjs
@@ -1,14 +1,20 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
+  env: {
+    es2020: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
