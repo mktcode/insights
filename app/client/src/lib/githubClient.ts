208bfdd045e62e3bef439a1467f8fe15efb69ef2
mktcode <kontakt@markus-kottlaender.de>
2023-02-23 16:03:14 +0100

    removed unused stuff

--- app/client/src/lib/githubClient.ts
+++
@@ -1,7 +0,0 @@
-import { graphql } from "@octokit/graphql";
-
-export const graphqlWithAuth = graphql.defaults({
-  headers: {
-    Authorization: `bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
-  },
-});
