aef75ddaa9ccc1590e44f469e9e55a7fbdba1d45
mktcode <kontakt@markus-kottlaender.de>
2023-02-24 15:46:19 +0100

    implemented animated chart

--- app/client/src/components/TeamCardActivityChart.vue
+++ app/client/src/components/TeamCardActivityChart.vue
@@ -1,18 +1,78 @@
-<template>
-<svg viewBox="0 0 150 30" xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color: #ffffff; stop-opacity: 1" />
-      <stop offset="100%" style="stop-color: #ffffff; stop-opacity: 0.2" />
-    </linearGradient>
-    <linearGradient id="areaGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color: #90ee90; stop-opacity: 0.5" />
-      <stop offset="100%" style="stop-color: #90ee90; stop-opacity: 0.2" />
-    </linearGradient>
-  </defs>
-  <rect x="0" y="0" width="150" height="30" fill="url(#gradient)" />
-  <path d="M 0 20 L 25 18 L 50 23 L 75 20 L 100 24 L 125 18 L 150 15" stroke="green" stroke-width="1" fill="none" />
-  <path d="M 0 20 L 25 18 L 50 23 L 75 20 L 100 24 L 125 18 L 150 15 L 150 30 L 0 30 Z" fill="url(#areaGradient)" />
-</svg>
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler,
+  type ChartOptions,
+  type ChartData
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler
+)
+
+const data: ChartData<"line"> = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      data: [1.5, 2, 2.5, 2, 3, 2, 2.8],
+      backgroundColor: 'rgba(0, 255, 0, 0.1)',
+      borderColor: 'green',
+      borderWidth: 2,
+      pointBorderWidth: 0,
+      pointRadius: 0,
+      pointHitRadius: 25,
+      pointBackgroundColor: 'green',
+      fill: true,
+      tension: 0.2
+    }
+  ]
+}
+
+const options: ChartOptions<"line"> = {
+  aspectRatio: 7,
+  layout: {
+    padding: {
+      top: 5,
+      bottom: 0,
+      left: 0,
+      right: 0,
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    },
+    title: {
+      display: false
+    },
+    filler: {
+      drawTime: 'beforeDatasetsDraw',
+      propagate: true
+    }
+  },
+  scales: {
+    x: {
+      display: false,
+    },
+    y: {
+      display: false,
+    }
+  }
+}
+
+</script>
+
+<template>
+  <Line :data="data" :options="options" />
 </template>
