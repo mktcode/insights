c9e8f06f9d21ec408855e31e339685be7ad90f1d
mktcode <kontakt@markus-kottlaender.de>
2023-02-27 18:35:58 +0100

    added paginator function

---
+++ packages/github-insights/src/paginate.ts
@@ -0,0 +1,31 @@
+import { DocumentNode, print } from "graphql";
+import { get, set } from "lodash";
+import { graphql } from "@octokit/graphql/dist-types/types";
+
+interface PaginatableQueryVariables {
+  first: number,
+  [key: string]: number | string | boolean,
+}
+
+export async function paginate<T extends Record<string, any>>(
+  client: graphql,
+  query: DocumentNode,
+  variables: PaginatableQueryVariables,
+  pathToPaginatedProperty: string[],
+): Promise<T> {
+  const fullData = await client<T>(print(query), variables);
+  
+  const { nodes, totalCount, pageInfo: { hasNextPage, endCursor } } = get(fullData, pathToPaginatedProperty);
+
+  if (hasNextPage) {
+    const perPage = Math.min(totalCount - nodes.length, 100);
+    const nextData = await paginate(client, query, { ...variables, first: perPage, after: endCursor }, pathToPaginatedProperty);
+    const nextPageData = get(nextData, pathToPaginatedProperty);
+    set(fullData, pathToPaginatedProperty, {
+      ...nextPageData,
+      nodes: [...nodes, ...nextPageData.nodes],
+    });
+  }
+
+  return fullData;
+}
